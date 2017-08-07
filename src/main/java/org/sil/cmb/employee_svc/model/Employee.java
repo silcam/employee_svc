398600c680866c605a03c1ac6ce28819eb15d7dc
Brian Yee <brian_yee@sil.org>
2017-08-07 11:03:13 +0100

    Additional detail in the Employee class.
    * Perhaps too much?
    * Need to verify persistence of new fields.

--- src/main/java/org/sil/cmb/employee_svc/model/Employee.java
+++ src/main/java/org/sil/cmb/employee_svc/model/Employee.java
@@ -1,7 +1,10 @@
 package org.sil.cmb.employee_svc.model;
 
+import org.joda.time.DateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.ArrayList;
 
 @Entity
 public class Employee {
@@ -12,13 +15,12 @@ public class Employee {
     private EmploymentStatus status;
     private String title;
     private String department;
-//    private Employee supervisor;
-//    private Gender gender;
-//    private String birthDate;
-//    private String CNPSno;
-//    private MaritalStatus martialStatus;
-//    private String[] children;
-//    private boolean isStudent;
+    private Employee supervisor;
+    private Gender gender;
+    private DateTime birthDate;
+    private String CNPSno;
+    private MaritalStatus martialStatus;
+    private ArrayList<String> children;
 
     public Employee(String id, String name) {
         this.name = name;
@@ -32,7 +34,6 @@ public class Employee {
     public String getName() {
         return this.name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -40,7 +41,6 @@ public class Employee {
     public EmploymentStatus getStatus() {
         return this.status;
     }
-
     public void setStatus(EmploymentStatus status) {
         this.status = status;
     }
@@ -48,9 +48,51 @@ public class Employee {
     public String getTitle() {
         return this.title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getDepartment() { return this.department; }
+    public void setDepartment(String department) { this.department = department; }
+
+    // TODO: Person
+    public Employee getSupervisor() { return this.supervisor; }
+    public void setSupervisor(Employee supervisor) { this.supervisor = supervisor; }
+
+    public Gender getGender() { return this.gender; }
+    public void setGender(Gender gender) { this.gender = gender; }
+
+    // TODO: is this the correct API for this?
+    public String getBirthDate() {
+        if (this.birthDate == null) {
+            return null;
+        } else {
+            return this.birthDate.toString();
+        }
+    }
+    public void setBirthDate(String birthDate) {
+        this.birthDate = new DateTime(birthDate);
+    }
+    public void setBirthDate(DateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCNPSno() { return this.CNPSno; }
+    public void setCNPSno(String CNPSno) { this.CNPSno = CNPSno; }
+
+    public MaritalStatus getMartialStatus() { return this.martialStatus; }
+    public void setMartialStatus(MaritalStatus martialStatus) { this.martialStatus = martialStatus; }
+
+    // TODO: Person
+    // TODO: Fix This.
+    public String[] getChildren() {
+        if (this.children == null) {
+            this.children = new ArrayList<String>();
+        }
+        return this.children.toArray(new String[this.children.size()]);
+    }
+    public void addChild(String childName) {
+        String[] childrenArray = this.getChildren();
+        this.children.add(childName);
+    }
 }
--- src/test/java/org/sil/cmb/employee_svc/model/EmployeeTest.java
+++ src/test/java/org/sil/cmb/employee_svc/model/EmployeeTest.java
@@ -1,8 +1,11 @@
 package org.sil.cmb.employee_svc.model;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.sil.cmb.employee_svc.model.Employee;
 import org.sil.cmb.employee_svc.model.EmploymentStatus;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -44,5 +47,84 @@ public class EmployeeTest {
         assertNull(employee.getStatus());
         employee.setStatus(EmploymentStatus.FULL_TIME);
         assertEquals(EmploymentStatus.FULL_TIME, employee.getStatus());
+    }
+
+    @Test
+    public void testDepartment() {
+        String expectedDepartment = "Computer Services";
+        Employee employee = new Employee("id", "Employee Name");
+        assertNull(employee.getDepartment());
+        employee.setDepartment(expectedDepartment);
+        assertEquals(expectedDepartment, employee.getDepartment());
+    }
+
+    @Test
+    public void testSupervisor() {
+        Employee supervisor = new Employee("id1", "Joe Supervisor");
+        Employee employee = new Employee("id", "Employee Name");
+        assertNull(employee.getSupervisor());
+        employee.setSupervisor(supervisor);
+        // not necessarily assertSame
+        // TODO: equals() on employee object?
+        assertEquals(supervisor.getId(), employee.getSupervisor().getId());
+    }
+
+    @Test
+    public void testGender() {
+        Employee employee = new Employee("id", "Employee Name");
+        assertNull(employee.getGender());
+        employee.setGender(Gender.FEMALE);
+        assertEquals(Gender.FEMALE, employee.getGender());
+    }
+
+    @Test
+    public void testBirthDate() {
+        String expectedBirthDate = "2016-01-01";
+        DateTime jodaDate = new DateTime(expectedBirthDate);
+
+        Employee employee = new Employee("id", "Employee Name");
+        assertNull(employee.getBirthDate());
+        employee.setBirthDate(jodaDate);
+        assertEquals(jodaDate.toString(), employee.getBirthDate());
+
+        String newExpectedBirthdate = "2001-01-31";
+        DateTime newJodaDate = new DateTime(newExpectedBirthdate);
+        employee.setBirthDate(newExpectedBirthdate);
+        assertEquals(newJodaDate.toString(), employee.getBirthDate());
+    }
+
+    @Test
+    public void testCNPSno() {
+        String expectedCNPSno = "123456";
+        Employee employee = new Employee("id", "Employee Name");
+        assertNull(employee.getCNPSno());
+        employee.setCNPSno(expectedCNPSno);
+        assertNotNull(expectedCNPSno, employee.getCNPSno());
+    }
+
+    @Test
+    public void testMartialStatus() {
+        Employee employee = new Employee("id", "Employee Name");
+        assertNull(employee.getMartialStatus());
+        employee.setMartialStatus(MaritalStatus.SINGLE);
+        assertEquals(MaritalStatus.SINGLE, employee.getMartialStatus());
+    }
+
+    @Test
+    public void testChildren() {
+        String expectedChildName = "child1";
+        Employee employee = new Employee("id", "Employee Name");
+
+        String [] children = employee.getChildren();
+
+        assertNotNull(children);
+        assertEquals(0, children.length);
+
+        employee.addChild(expectedChildName);
+
+        children = employee.getChildren();
+
+        assertEquals(1, children.length);
+        assertEquals(expectedChildName, children[0]);
     }
 }
